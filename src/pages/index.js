41716c3cde8f8cc448615732cd70700eb5af9c3f
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-05-26 17:24:30 +0200

    index: display .md posts on first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,51 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout>
-      <div>Hello world!</div>
+      <div>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3
+              css={css`
+                margin-bottom: ${rhythm(1 / 4)};
+              `}
+            >
+              {node.frontmatter.title}{" "}
+              <span
+                css={css`
+                  color: #bbb;
+                `}
+              >
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
     </Layout>
   )
 }
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
