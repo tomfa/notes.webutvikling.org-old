0d6b15c26fd3a90bc9de30a80ac6c52edec46ec4
Tomas Fagerbekk <tomas@otovo.com>
2020-05-26 15:20:30 +0200

    Layout component: Add StaticQuery use

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,20 @@
 import React from "react"
 import { css } from "@emotion/core"
-import { Link } from "gatsby"
+import { useStaticQuery, Link, graphql } from "gatsby"
 
 import { rhythm } from "../utils/typography"
-
 export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
   return (
     <div
       css={css`
@@ -22,7 +32,7 @@ export default function Layout({ children }) {
             font-style: normal;
           `}
         >
-          Notes and Anecdotes
+          {data.site.siteMetadata.title}
         </h3>
       </Link>
       <Link
