3b132b35015b07c99abc90065ad35963ab9606e4
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-05-26 22:58:03 +0200

    Add prism-react-renderer

---
+++ gatsby-browser.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { MDXProvider } from "@mdx-js/react"
+import { SyntaxHighLighter } from "./src/components/SyntaxHighlighter"
+
+const components = {
+  wrapper: ({ children }) => <>{children}</>,
+  pre: SyntaxHighLighter,
+}
+
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>
+}
---
+++ src/components/SyntaxHighlighter.js
@@ -0,0 +1,39 @@
+import React from "react"
+import Highlight, { defaultProps } from "prism-react-renderer"
+import theme from "prism-react-renderer/themes/nightOwl"
+
+const getLanguage = props => {
+  const className = props.children.props.className || ""
+  const matches = className.match(/language-(?<lang>.*)/)
+  const language =
+    matches && matches.groups && matches.groups.lang ? matches.groups.lang : ""
+  return language
+}
+
+const getCode = props => props.children.props.children
+
+export const SyntaxHighLighter = props => {
+  return (
+    <Highlight
+      {...defaultProps}
+      theme={theme}
+      code={getCode(props)}
+      language={getLanguage(props)}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre
+          className={className}
+          style={{ padding: "1rem", paddingBottom: 0, ...style }}
+        >
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
