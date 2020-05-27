ea71b40fcaac69b89a57aa013fa6f959e2ddd254
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-05-27 13:10:14 +0200

    style: style Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,12 +2,43 @@ import React from "react"
 import styled from "styled-components"
 import { useStaticQuery, graphql, Link } from "gatsby"
 
-const Nav = styled.nav``
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+  > ul {
+    display: none;
+  }
+
+  @media (min-width: 800px) {
+    > ul {
+      display: inline-block;
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+    }
+  }
+`
+
+const NavItem = styled.li`
+  font-size: 1.3rem;
+  display: inline-block;
+  margin-left: 2rem;
+  & a {
+    color: white;
+  }
+`
+
+const NavItemLink = ({ children, to }) => (
+  <NavItem>
+    <Link to={to}>{children}</Link>
+  </NavItem>
+)
 
 const HomeLink = styled(Link)`
   font-size: 1.4rem;
   color: white;
-  text-decoration: none;
 `
 
 export const Navigation = () => {
@@ -23,11 +54,11 @@ export const Navigation = () => {
     `
   )
   return (
-    <>
-      <HomeLink to={`/`}>{data.site.siteMetadata.title}</HomeLink>
-      <Nav>
-        <Link to={`/about/`}>About</Link>
-      </Nav>
-    </>
+    <Nav>
+      <HomeLink to={`/`}>{title}</HomeLink>
+      <ul>
+        <NavItemLink to={`/about/`}>About</NavItemLink>
+      </ul>
+    </Nav>
   )
 }
