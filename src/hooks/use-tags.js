aecf6ce95edc6f55792c08699f179086a376dd33
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-06-09 08:42:21 +0200

    useTags: add tag count

--- src/hooks/use-tags.js
+++ src/hooks/use-tags.js
@@ -6,14 +6,34 @@ export const useTags = () => {
     graphql`
       query allCategories {
         allMdx {
+          edges {
+            node {
+              frontmatter {
+                tags
+              }
+            }
+          }
           distinct(field: frontmatter___tags)
         }
       }
     `
   )
   const tags = data.allMdx.distinct
-  return tags.map(tag => ({
-    tag,
-    url: `/tag/${asUrl(tag)}`,
-  }))
+  const tagCount = tags.reduce((obj, item) => {
+    obj[item] = 0
+    return obj
+  }, {})
+  const edges = data.allMdx.edges || []
+  edges.forEach(edge => {
+    const tags = (edge.node.frontmatter && edge.node.frontmatter.tags) || []
+    tags.forEach(tag => (tagCount[tag] += 1))
+  })
+
+  return tags
+    .map(tag => ({
+      tag,
+      url: `/tag/${asUrl(tag)}`,
+      count: tagCount[tag],
+    }))
+    .sort((a, b) => b.count - a.count)
 }
