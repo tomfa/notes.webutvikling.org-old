ea883eceb66f594a62150f711d3eb834822de6cd
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-06-12 08:44:37 +0200

    Footer: replace tag lists with some basic info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,36 +9,77 @@ const FooterTag = styled.footer`
   padding: 2rem;
   margin-bottom: 0;
   min-height: 10rem;
+  display: grid;
+  grid-template-areas:
+    "what"
+    "whoami";
+  grid-gap: 1rem;
+
+  @media (min-width: 800px) {
+    grid-template-areas: "what whoami";
+  }
 `
 
-const LinkList = styled.ul`
+const Div = styled.div`
+  grid-area: ${props => props.ga};
+`
+
+const List = styled.ul`
   list-style: none;
   color: white;
   padding: 0;
-  font-size: 1rem;
+  margin: 0;
+
+  & > li {
+    line-height: "1.5rem";
+    font-size: "1rem";
+  }
 `
 
 const ListItem = styled.li`
-  line-height: 1.5em;
   & > a {
     color: white;
   }
+
+  & > a:hover,
+  & > a:focus {
+    color: #7478ef;
+  }
+`
+
+const FooterHeader = styled.h3`
+  margin-top: 0;
+  font-size: 2rem;
+  color: rgb(214, 222, 235);
+  background-color: #3a3a3a;
 `
 
 export const Footer = () => {
   const tags = useTags().slice(0, 12)
   return (
-    <>
-      <FooterTag>
-        <h3>Tags</h3>
-        <LinkList>
-          {tags.map(({ tag, url }) => (
-            <ListItem>
-              <Link to={url}>{tag}</Link>
-            </ListItem>
-          ))}
-        </LinkList>
-      </FooterTag>
-    </>
+    <FooterTag>
+      <Div area="what">
+        <FooterHeader>What</FooterHeader>
+        <List>
+          <ListItem>These are just notes and scribbl.</ListItem>
+          <ListItem>Don't take anything seriously.</ListItem>
+          <ListItem>Usually I don't know what I'm talking about.</ListItem>
+        </List>
+      </Div>
+      <Div area="whoami">
+        <FooterHeader>Me</FooterHeader>
+        <List>
+          <ListItem>
+            <a href="https://github.com/tomfa">Github</a>
+          </ListItem>
+          <ListItem>
+            <a href="https://twitter.com/tommfa">Twitter</a>
+          </ListItem>
+          <ListItem>
+            <a href="https://www.linkedin.com/in/fagerbekk/">LinkedIn</a>
+          </ListItem>
+        </List>
+      </Div>
+    </FooterTag>
   )
 }
