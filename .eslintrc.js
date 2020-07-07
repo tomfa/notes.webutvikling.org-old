a05231d1b9b299b7f0749a4b01699be8cf3f6d4e
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-07-07 02:04:33 +0200

    eslint: add .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -14,9 +11,17 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
+    // Prettier responsibility
+    'comma-dangle': 'off',
+    'arrow-parens': 'off',
+    'operator-linebreak': 'off',
+    'object-curly-newline': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+
+    // https://notes.webutvikling.org/default-exports/
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
