7db4245225df665f10b75c01cb07a460ab4a798b
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-05-29 21:57:32 +0200

    Upgrade linting packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['@askeladden/eslint-config-askeladden'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
